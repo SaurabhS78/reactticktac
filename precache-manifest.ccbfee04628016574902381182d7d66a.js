29531974798ae374d9775fd84fdbd12942c5a7b2
SaurabhS78 <saurabhsangam78@gmail.com>
2020-07-28 02:41:33 +0530

    Updates

---
+++ precache-manifest.ccbfee04628016574902381182d7d66a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5321b1593e91b383ea5978179be749ca",
+    "url": "/reactticktac/index.html"
+  },
+  {
+    "revision": "bda3ddddb8ea84b9db87",
+    "url": "/reactticktac/static/css/main.00cd9408.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/reactticktac/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactticktac/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda3ddddb8ea84b9db87",
+    "url": "/reactticktac/static/js/main.cbd0774e.chunk.js"
+  },
+  {
+    "revision": "fb9a14f0b4bcc1f0a60c",
+    "url": "/reactticktac/static/js/runtime-main.b85b1ff8.js"
+  }
+]);
